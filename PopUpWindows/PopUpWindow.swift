62979b8082e9c58688491328e92926dd81f0bcd5
adiveli <adi.veli@ymail.com>
2018-10-30 11:54:56 +0200

    Services Container Implementation

---
+++ PopUpWindows/PopUpWindow.swift
@@ -0,0 +1,30 @@
+//
+//  PopUpWindow.swift
+//  PopUpWindows
+//
+//  Created by Adi Veliman on 30/10/2018.
+//  Copyright © 2018 Adi Veliman. All rights reserved.
+//
+
+import UIKit
+
+class PopUpWindow: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
